3dd3ce699a581afe3a0ab03d26c667a039b6f3ab
tuyenlt <keplerlu@gmail.com>
2025-06-16 22:19:06 +0700

    update cors

--- src/configs/corsConfig.js
+++ src/configs/corsConfig.js
@@ -2,16 +2,15 @@ const cors = require('cors')
 
 const allowedList = [
 	"http://localhost:5173",
-	"http://tmsherk.id.vn",
-	"http://api.tmsherk.id.vn",
-	"http://englishnest.click",
-	"http://api.englishnest.click",
+	"https://tmsherk.id.vn",
+	"https://api.tmsherk.id.vn",
+	"https://englishnest.click",
+	"https://api.englishnest.click",
 	"https://englishnest.click",
 	"https://api.englishnest.click",
 ]
 
 const apiCors = cors({
-	// origin: "http://localhost:5173",
 	origin: function (origin, callback) {
 		if (!origin) return callback(null, true);
 		if (origin.includes("http://localhost")) {
@@ -22,13 +21,7 @@ const apiCors = cors({
 			return callback(null, true);
 		}
 
-		const allowedRegex = /^http:\/\/192\.168\.\d{1,3}\.\d{1,3}:5173$/;
-
-		if (allowedRegex.test(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
+		callback(new Error("Not allowed by CORS"));
 	},
 	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 	credentials: true,
@@ -36,23 +29,17 @@ const apiCors = cors({
 
 
 const socketCors = {
-	// origin: 'http://localhost:5173',
 	origin: function (origin, callback) {
 		if (!origin) return callback(null, true);
 		if (origin.includes("http://localhost")) {
 			return callback(null, true);
 		}
-		const allowedRegex = /^http:\/\/192\.168\.\d{1,3}\.\d{1,3}:5173$/;
 
 		if (allowedList.includes(origin)) {
 			return callback(null, true);
 		}
 
-		if (allowedRegex.test(origin)) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-		}
+		callback(new Error("Not allowed by CORS"));
 	},
 	methods: ['GET', 'POST']
 }
